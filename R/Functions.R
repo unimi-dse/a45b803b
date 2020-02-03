8d9ab229e0e58d0a3d2e50f207c5b409f6401dcc
Filippo Menegatti <59975495+filippomenegatti@users.noreply.github.com>
2020-02-03 22:38:28 +0100

    Add files via upload

--- R/Functions.R
+++ R/Functions.R
@@ -1,6 +1,7 @@
 #load the dataset from github
 loaddata <- function(){
-  DATA <- read.csv(file=system.file("extdata", "DATA.csv", package="TS1"), sep = ',')
+  DATA <- read.csv('https://raw.githubusercontent.com/unimi-dse/a45b803b/master/extinst/extdata/DATA.csv')
+  DATA$DATE <- as.Date.factor(DATA$DATE)
   return(DATA)
 }
 
