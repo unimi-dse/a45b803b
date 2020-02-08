bda8d8e48f08a0abb5650efc2abdc960079b146d
Filippo Menegatti <59975495+filippomenegatti@users.noreply.github.com>
2020-02-08 22:38:04 +0100

    Add files via upload

---
+++ R/DATA.R
@@ -0,0 +1,12 @@
+#' US GDP per capita and Inflation data.
+#'
+#' This dataset contains 287 quarterly observation of US GDP per capita and inflation percentage change from the year before, starting from 1948-01-01 and ending 2019-07-01.
+#'
+#' @format A data frame with 287 rows and 3 variables:
+#' \describe{
+#'   \item{DATA}{Dates in quarters from 1948-01-01 until 2019-07-01}
+#'   \item{GDP_PERCAPITA}{US GDP per capita, Chained 2012 Dollars, Seasonally Adjusted Annual Rate}
+#'   \item{INFLATION}{Consumer Price Index for All Urban Consumers: All Items in U.S. City Average, Percent Change from Year Ago, Seasonally Adjusted}
+#' }
+#' @source \url{https://fred.stlouisfed.org//}
+"DATA"
